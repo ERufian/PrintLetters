6621344bd200391825ebffde4dbe8ad65871c396
ERufian <github@rufian.zilbermann.com>
2015-01-15 15:22:09 -0500

    Version 1.1
    New version without using recursion or stacks. Count in base 4.

--- PrintLetters.cs
+++ PrintLetters.cs
@@ -17,29 +17,32 @@ namespace PrintLetters
       /// <summary>
       /// The table for possible conversions from a digit to a letter.
       /// </summary>
-      private static readonly string[][] ConversionTable = new string[10][];
+      private static readonly char[,] ConversionTable = new char[10, 4];
 
       /// <summary>
       /// Initializes static members of the <see cref="PrintLetters"/> class.
       /// </summary>
       static PrintLetters()
       {
-         PrintLetters.ConversionTable[0] = new string[] { "0" };
-         PrintLetters.ConversionTable[1] = new string[] { "1" };
-         PrintLetters.ConversionTable[2] = new string[] { "A", "B", "C" };
-         PrintLetters.ConversionTable[3] = new string[] { "D", "E", "F" };
-         PrintLetters.ConversionTable[4] = new string[] { "G", "H", "I" };
-         PrintLetters.ConversionTable[5] = new string[] { "J", "K", "L" };
-         PrintLetters.ConversionTable[6] = new string[] { "M", "N", "O" };
-         PrintLetters.ConversionTable[7] = new string[] { "P", "Q", "R", "S" };
-         PrintLetters.ConversionTable[8] = new string[] { "T", "U", "V" };
-         PrintLetters.ConversionTable[9] = new string[] { "W", "X", "Y", "Z" };
+         PrintLetters.ConversionTable = new char[10, 4] 
+         { 
+            { '0', ':', ':', ':' },
+            { '1', ':', ':', ':' },
+            { 'A', 'B', 'C', ':' },
+            { 'D', 'E', 'F', ':' },
+            { 'G', 'H', 'I', ':' },
+            { 'J', 'K', 'L', ':' },
+            { 'M', 'N', 'O', ':' },
+            { 'P', 'Q', 'R', 'S' },
+            { 'T', 'U', 'V', ':' },
+            { 'W', 'X', 'Y', 'Z' }
+         };
       }
 
       /// <summary>
       /// The main executable method.
       /// Parse the input and then convert the last 7 digits out of the 10
-      /// (traditionally the area code does not get converted).
+      /// (follow the tradition where the area code does not get converted if it is 1-800 or 1-888).
       /// </summary>
       /// <param name="args">The input arguments.</param>
       public static void Main(string[] args)
@@ -47,6 +50,7 @@ namespace PrintLetters
          if (1 != args.Length || null == args[0] || string.IsNullOrEmpty(args[0]))
          {
             Console.WriteLine("Usage: PrintLetter <10 digit phone number as nnnnnnnnnn or n-nnn-nnn-nnnn>");
+            return;
          }
 
          string phoneNumberToConvert = args[0];
@@ -56,37 +60,78 @@ namespace PrintLetters
             && (13 == phoneNumberToConvert.Length && withHyphen.IsMatch(phoneNumberToConvert)))
          {
             Console.WriteLine("Specify the 10 digit phone number as nnnnnnnnnn or n-nnn-nnn-nnnn");
-         }
-
-         phoneNumberToConvert = phoneNumberToConvert.Replace("-", string.Empty);
-         StringBuilder sb = new StringBuilder(phoneNumberToConvert.Substring(0, 4), 10);
-         Convert(phoneNumberToConvert.ToCharArray(), sb);
-      }
-
-      /// <summary>
-      /// Recursively convert digits one-by-one and print out the result when finished.
-      /// </summary>
-      /// <param name="inputNumbers">The original input number as a char array of 10 digits.</param>
-      /// <param name="sb">The partial result.</param>
-      public static void Convert(char[] inputNumbers, StringBuilder sb)
-      {
-         if (sb.Length == inputNumbers.Length)
-         {
-            string result = sb.ToString();
-            Console.WriteLine(
-               "{0}-{1}-{2}-{3}", 
-               result.Substring(0, 1), 
-               result.Substring(1, 3), 
-               result.Substring(4, 3), 
-               result.Substring(7, 4));
             return;
          }
 
-         for (int i = 0; ConversionTable[inputNumbers[sb.Length] - '0'].Length > i; i++)
+         phoneNumberToConvert = phoneNumberToConvert.Replace("-", string.Empty);
+         string printPrefix = string.Empty;
+
+         if (0 == string.Compare(phoneNumberToConvert, 0, "1800", 0, 4, StringComparison.OrdinalIgnoreCase))
          {
-            sb.Append(ConversionTable[inputNumbers[sb.Length] - '0'][i]);
-            Convert(inputNumbers, sb);
-            sb.Remove(sb.Length - 1, 1);
+            phoneNumberToConvert = phoneNumberToConvert.Substring(4);
+            printPrefix = "1-800-";
+         }
+         else if (0 == string.Compare(phoneNumberToConvert, 0, "1888", 0, 4, StringComparison.OrdinalIgnoreCase))
+         {
+            phoneNumberToConvert = phoneNumberToConvert.Substring(4);
+            printPrefix = "1-888-";
+         }
+            
+         Convert(phoneNumberToConvert.ToCharArray(), printPrefix);
+      }
+
+      /// <summary>
+      /// Use a counter to produce all possible combinations and convert digits.
+      /// </summary>
+      /// <param name="inputNumbers">The original input number as a char array of 10 digits</param>
+      /// <param name="printPrefix">An optional prefix to print first (this can be used for cases like 1-800-nnnnnnn where the 1-800 is not converted).</param>
+      /// <remarks>
+      /// The conversion table has up to 4 items, this routine uses bit manipulation to count in base 4
+      /// </remarks>
+      public static void Convert(char[] inputNumbers, string printPrefix)
+      {
+         for (int i = 0; inputNumbers.Length > i; ++i)
+         {
+            inputNumbers[i] -= '0';
+         }
+         
+         // Upper limit for counter: 2 bits per digit
+         long upperLimit = 1 << (inputNumbers.Length << 1); // Use long to support the full range of 10 digits if needed
+         bool printResult = true;
+         
+         // Optimize inputs of the form 1-nnn... where we can count in increments of 4
+         int counterIncrement = (inputNumbers[0] == 1) ? 4 : 1;
+         for (long counter = 0; upperLimit > counter; counter += counterIncrement)
+         {
+            char[] candidateBuilder = new char[inputNumbers.Length];
+
+            for (int j = 0; inputNumbers.Length > j; ++j)
+            {
+               // break up counter into pairs of 2 bits
+               char index = (char)(counter >> (j << 1) & 3);
+
+               char appendCandidate = PrintLetters.ConversionTable[inputNumbers[j], index];
+               if (appendCandidate != ':')
+               {
+                  candidateBuilder[j] = appendCandidate;
+               }
+               else
+               {
+                  printResult = false;
+                  break;
+               }
+            }
+
+            if (printResult)
+            {
+               StringBuilder result = new StringBuilder(printPrefix);
+               result.Append(candidateBuilder);
+               Console.WriteLine(result);
+            }
+            else
+            {
+               printResult = true;
+            }
          }
       }
    }
--- README.md
+++ README.md
@@ -5,14 +5,28 @@ Print mnemonic combinations of 10-digit telephone numbers. These are combination
 that are easier to remember than 10 digits (using letters as shown on a telephone keyboard)
 
 The portions that remain as numbers are:
-* The country and area codes (traditionally left as is, for example 1-800)
+* The country and area codes if they are of the form 1-800 or 1-888
 * The digits 1 and 0
 
 The remaining digits are converted to letters and all the possible combinations are printed out
 
-A couple of days ago I was asked this question at a phone interview. I overcomplicated the answer and didn't get anywhere, 
-which was tremendously annoying. After the interview I decided to write it out as an exercise anyway, just for my own sake.
+Some time ago ago I was asked this question at a phone interview. I overcomplicated the answer and didn't get anywhere, 
+which was tremendously annoying. After the interview I decided to write it out as an exercise.
 
-Maybe someone actually has uses for this kind of functionality so I decided to upload it to Github.
-Feel free to re-use it. I am releasing this code under a choice of MIT License or GPL v2 (your choice, pick one or the other). 
-If you need different terms please let me know.
+The result was interesting and I thought that maybe someone actually has uses for this kind of functionality,
+so I decided to upload it to Github.
+
+Some time later, on an interview for a different company I was challenged to optimize my old code and come up with a 
+more efficient solution that didn't require using a stack (implicitly, as recursion does or explicitly). The original 
+interviewer was guiding me towards a recursive solution, which is what I first implemented. Recursion is interesting 
+from a theoretical point of view and often used in interviews to see if the candidate can think recursively, however
+when moving from theory to practice sometimes it can lead to surprisingly inefficient code. Because of the function
+calls and all the stack manipulation, if there is a non-recursive solution _of similar complexity_ it is often faster.
+
+This problem can be solved with a straight loop. We can either count in base 4 and skip numbers as needed (presented here)
+or optimize it further using cascading counters and modulo operations for each counter (left as an exercise ;)
+
+Note: the original recursive solution is still available, switch to the "RecursiveSolution" branch if yoou're curious). 
+
+Feel free to re-use any of this code. I am releasing it under a choice of MIT License or GPL v2 (your choice, pick one 
+or the other). If you need different terms please let me know.
